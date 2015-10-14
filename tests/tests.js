bed43e20967f24e32b5f64df907aa5a3f182dd88
Francisco Mendoza González <alu0100600582@ull.edu.es>
2015-10-14 15:28:10 +0100

    Añadiendo primeras pruebas BDD

--- tests/tests.js
+++ tests/tests.js
@@ -1,33 +1,17 @@
-var assert = chai.assert;
+var expect = chai.expect;
 
-suite('temperature', function() {
-    test('32F = 0C', function() {
-        original.value = "32F";
-        calculate();
-        assert.deepEqual(converted.innerHTML, "0 Celsius");
-    });
-    test('45C = 113.0 Farenheit', function() {
-        original.value = "45C";
-        calculate();
-        assert.deepEqual(converted.innerHTML, "113 Farenheit");
-    });
+describe("Test BDD para el Conversor de Temperatura", function() {
 
-    test('5X = error', function() {
-        original.value = "5X";
-        calculate();
-        assert.match(converted.innerHTML, /ERROR/);
+    it("Debería ser: 32e4F", function() {
+      var temp = new Temperatura(32,4,"F");
+      expect(temp.get_valor()).to.equal(32);
+      expect(temp.get_exp()).to.equal(4);
+      expect(temp.get_tipo()).to.equal("F");
     });
-
-    test('-45C = -49.0 Farenheit', function() {
-        original.value = "-45C";
-        calculate();
-        assert.deepEqual(converted.innerHTML, "-49 Farenheit");
+    it("Debería ser: -4.3e-2C", function() {
+      var temp = new Temperatura(-4.3,-2,"C");
+      expect(temp.get_valor()).to.equal(-4.3);
+      expect(temp.get_exp()).to.equal(-2);
+      expect(temp.get_tipo()).to.equal("C");
     });
-
-    test('converted == String', function() {
-       original.value = "-54.23F";
-       calculate();
-       assert.isString(converted.innerHTML);
-    });
-
 });
