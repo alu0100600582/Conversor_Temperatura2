ff646cabe5ec92d98750305ccc392d4ce02866bb
Francisco Mendoza González <alu0100600582@ull.edu.es>
2015-10-14 13:36:37 +0100

    Clonando Práctica Anterior

---
+++ README.md
@@ -0,0 +1,19 @@
+Práctica: Conversor en JavaScript de grados Celsius a Farenheit y viceversa
+
+Que aprendemos
+
+    HTML básico
+    JavaScript básico
+    Expresiones Regulares
+    Un poquito de CSS
+    Uso de Emmet (instale el plugin para su editor)
+    Uso de Mocha y Chai
+    Uso de Gulp
+
+Repositorio Github: https://github.com/alu0100600582/Conversor_Temperatura
+
+GitHub page de la aplicación: http://alu0100600582.github.io/Conversor_Temperatura/
+
+GitHub page de las pruebas: http://alu0100600582.github.io/Conversor_Temperatura/tests/index
+
+Mi GitHub page: http://alu0100600582.github.io/
---
+++ tests/tests.js
@@ -0,0 +1,33 @@
+var assert = chai.assert;
+
+suite('temperature', function() {
+    test('32F = 0C', function() {
+        original.value = "32F";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "0.0 Celsius");
+    });
+    test('45C = 113.0 Farenheit', function() {
+        original.value = "45C";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "113.0 Farenheit");
+    });
+
+    test('5X = error', function() {
+        original.value = "5X";
+        calculate();
+        assert.match(converted.innerHTML, /ERROR/);
+    });
+
+    test('-45C = -49.0 Farenheit', function() {
+        original.value = "-45C";
+        calculate();
+        assert.deepEqual(converted.innerHTML, "-49.0 Farenheit");
+    });
+
+    test('converted == String', function() {
+       original.value = "-54.23F";
+       calculate();
+       assert.isString(converted.innerHTML);
+    });
+    
+});
